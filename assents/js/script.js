d556401cc8b4a7b097252410337bf042ec7c9707
iamlzx7 <27994500a@gmail.com>
2023-03-07 16:08:43 -0300

    primeiro commit

---
+++ assents/js/script.js
@@ -0,0 +1,22 @@
+let count= 1;
+
+document.getElementById ("radio1") .checked = true
+
+setInterval(function() {
+ nextImage();
+}, 4500)
+
+
+function nextImage() {
+
+    count++;
+    if (count>4) {
+        count = 1;
+    }
+
+document.getElementById("radio"+count).checked = true;
+
+
+
+}
+
